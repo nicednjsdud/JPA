8c2f68abea73fb95a70ee4539a0648cf768fc742
JeongWonYoung <nicednjsdud@naver.com>
2022-09-10 01:31:09 +0900

    Docs JPA notes
    데이터 베이스 스키마 자동 생성

--- jpa-example/ex1hellojpa/src/main/java/hellojpa/Member.java
+++ jpa-example/ex1hellojpa/src/main/java/hellojpa/Member.java
@@ -1,15 +1,17 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="MBR")
 public class Member {
 
     @Id
     private Long id;
+
+    @Column(unique = true,length = 10)
     private String name;
 
     public Member(){}
