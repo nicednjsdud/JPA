8c93ba2032b2471342c72213c2f660c942577257
JeongWonYoung <nicednjsdud@naver.com>
2022-09-10 00:36:04 +0900

    feat method
    context example

--- jpa-example/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-example/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -38,20 +38,38 @@ public class JpaMain {
                     .setMaxResults(10)
                     .getResultList();
             // 비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
 
             // 영속
-            System.out.println("=== Before ===");
-            em.persist(member);
-
+//            System.out.println("=== Before ===");
+//            em.persist(member);
             // detach
-            em.detach(member);
-
+//            em.detach(member);
             // remove
-            em.remove(member);
-            System.out.println("=== After ===");
+//            em.remove(member);
+//            System.out.println("=== After ===");
+//            Member findMember = em.find(Member.class, 101L);
+//
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println("findMember.getName() = " + findMember.getName());
+
+            // 영속
+//            Member member1 = new Member(150L,"A");
+//            Member member2 = new Member(160L,"B");
+
+//            em.persist(member1);
+//            em.persist(member2);
+
+//            Member member = em.find(Member.class, 150L);
+//            member.setName("ZZZZZ");
+//            System.out.println("==============");
+
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+            em.flush();
+            System.out.println("================");
 
 
             tx.commit();
