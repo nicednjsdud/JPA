3dd05cb86b3dac91cc626155e7b07e9887e596ad
JeongWonYoung <nicednjsdud@naver.com>
2022-09-14 00:20:32 +0900

    Study about mapping

--- jpa-example/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-example/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -71,23 +71,42 @@ public class JpaMain {
 //            em.flush();
 //            System.out.println("================");
 
-            Member member1 = new Member();
-            member1.setUsername("A");
+//            Member member1 = new Member();
+//            member1.setUsername("A");
+//
+//            Member member2 = new Member();
+//            member2.setUsername("B");
+//
+//            Member member3 = new Member();
+//            member3.setUsername("C");
+//            System.out.println("============");
+//            em.persist(member1);
+//            em.persist(member2);
+//            em.persist(member3);
+//
+//            System.out.println("member1 = " + member1.getId());
+//            System.out.println("member2= "+member2.getId());
+//            System.out.println("member3.getId() = " + member3.getId());
+//            System.out.println("============");
 
-            Member member2 = new Member();
-            member2.setUsername("B");
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
-            Member member3 = new Member();
-            member3.setUsername("C");
-            System.out.println("============");
-            em.persist(member1);
-            em.persist(member2);
-            em.persist(member3);
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
 
-            System.out.println("member1 = " + member1.getId());
-            System.out.println("member2= "+member2.getId());
-            System.out.println("member3.getId() = " + member3.getId());
-            System.out.println("============");
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
 
             tx.commit();
--- jpa-example/ex1hellojpa/src/main/java/hellojpa/Member.java
+++ jpa-example/ex1hellojpa/src/main/java/hellojpa/Member.java
@@ -6,16 +6,20 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@SequenceGenerator(name="member_seq_generator",sequenceName = "member_seq",initialValue = 1,allocationSize = 50)
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "member_seq_generator")
+    @GeneratedValue
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member(){}
+//    @Column(name="TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name="TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -31,5 +35,13 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
