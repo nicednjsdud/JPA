b1656dc31a90d8b4f624420c896b6d8816961c07
JeongWonYoung <nicednjsdud@naver.com>
2022-09-17 23:41:58 +0900

    Build project jpql
    to study jpql

---
+++ jpa-example/jpabook/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Order order = new Order();
+//            order.addOrderItem(new OrderItem());
+
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("정원영");
+            em.persist(book);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
---
+++ jpa-example/jpabook/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+public class Delivery extends BaseEntity{
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Embedded
+    private Address address;
+
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery",fetch = LAZY)
+    private Order order;
+}
