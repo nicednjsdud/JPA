9483cdbd356fc32421f0691e7bf47f0d14097c4f
JeongWonYoung <nicednjsdud@naver.com>
2022-09-18 01:16:47 +0900

    Study about jpql

---
+++ jpa-example/jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,72 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m",MemberDTO.class)
+                      .getResultList();
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
+
+
+//            List<Object[]> resultList = em.createQuery("select m.username, m.age from Member m")
+//                    .getResultList();
+//            Object[] result = resultList.get(0);
+//            System.out.println("username = " + result[0]);  // member1
+//            System.out.println("age = " + result[1]);       // 10
+
+
+//            List resultList = em.createQuery("select m.username, m.age from Member m" )
+//                    .getResultList();
+//            Object o = resultList.get(0);
+//            Object[] result = (Object[]) o;
+//            System.out.println("username = " + result[0]);  // member1
+//            System.out.println("age = " + result[1]);       // 10
+
+
+//            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+//            List<Member> resultList = query.getResultList();
+//
+//            for (Member member1 : resultList) {
+//                System.out.println("member1 = " + member1);
+//            }
+//
+//            Member result = em.createQuery("select m from Member m where m.username =:username", Member.class)
+//                        .setParameter("username", "member1")
+//                        .getSingleResult();
+//            System.out.println("result = " + result);
+
+
+            // Spring Data JPA -> 
+//            System.out.println("resultList2 = " + resultList2);
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
