6102773554d4b1b260a4fc0d157c8ea330c06ed5
JeongWonYoung <nicednjsdud@naver.com>
2022-09-27 01:01:05 +0900

    Add BookForm
    add bookForm

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -47,7 +47,7 @@ public class MemberController {
     public String list(Model model) {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members",members);
-        
+
         return "members/memberList";
     }
 }
