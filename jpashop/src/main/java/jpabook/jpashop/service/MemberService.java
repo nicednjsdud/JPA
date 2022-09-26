ea4cb6bbea99098a2eec03a4b566e51e31eff612
JeongWonYoung <nicednjsdud@naver.com>
2022-09-26 11:34:43 +0900

    add @RequiredArgsContsructor
    to inject constructor

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,25 +2,31 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
 
-    @Autowired
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
-    /*
-        회원 가입
+    /**
+     *회원 가입
      */
+    @Transactional
     public Long join(Member member) {
         validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
         return member.getId();
     }
+
 
     private void validateDuplicateMember(Member member) {
         // exception
@@ -30,7 +36,16 @@ public class MemberService {
         }
 
     }
+
     // 회원 전체 조회
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
 
 
 }
