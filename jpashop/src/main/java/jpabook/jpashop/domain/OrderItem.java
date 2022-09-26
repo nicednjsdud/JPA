60552a9dfbc253ed0abb4e27a7324e238b685336
JeongWonYoung <nicednjsdud@naver.com>
2022-09-26 14:17:25 +0900

    feat OrderService
    add order and cancel logic

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import static javax.persistence.FetchType.LAZY;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -28,6 +31,7 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격
     private int count;  // 주문 수량
+
 
     // == 생성 메서드 == //
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
