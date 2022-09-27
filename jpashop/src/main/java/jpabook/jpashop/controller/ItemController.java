4209395aa61cbf4b0c3c80a202f5cbe2bb76b8fc
JeongWonYoung <nicednjsdud@naver.com>
2022-09-27 18:02:57 +0900

    Change Shop name

--- jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -84,7 +84,7 @@ public class ItemController {
 
         itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
 
-        return "redirect:items";
+        return "redirect:/items";
 
     }
 }
