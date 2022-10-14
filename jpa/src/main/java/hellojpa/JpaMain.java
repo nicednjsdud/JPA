248ff05fd96eeb97873ea4900eface8217c3fa9b
JeongWonYoung <nicednjsdud@naver.com>
2022-10-14 22:04:28 +0900

    Study persist

--- jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa/src/main/java/hellojpa/JpaMain.java
@@ -33,15 +33,32 @@ public class JpaMain {
 //                System.out.println("findMember = " + findMember);
 //            }
             // 비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
 
             // 영속
-            System.out.println("BEFORE");
-            em.persist(member);
-            System.out.println("AFTER");
+//            System.out.println("BEFORE");
+//            em.persist(member);
+//            System.out.println("AFTER");
+//
+//            Member findMember = em.find(Member.class, 101L);
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println("findMember.getName() = " + findMember.getName());
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
+//
+//            System.out.println("result = "+(findMember2 == findMember1));
 
+            // 영속
+//            Member member1 = new Member(150L,"A");
+//            Member member2 = new Member(160L,"B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+            Member findMember = em.find(Member.class, 150L);
+            findMember.setName("ZZZZZ");
+            
 
             tx.commit();
 
