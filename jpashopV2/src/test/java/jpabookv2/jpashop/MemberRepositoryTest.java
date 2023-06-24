9017d0077772ace5c80e9ea3e6adf8fe3564aa11
JeongWonYoung <nicednjsdud@naver.com>
2023-06-24 18:20:35 +0900

    멤버 저장 조회 테스트

---
+++ jpashopV2/src/test/java/jpabookv2/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package jpabookv2.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        Member member = new Member();
+        member.setUsername("BoB");
+
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
