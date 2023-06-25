0f347763c02dd917c6d63f27f9fe572beb83a4a4
JeongWonYoung <nicednjsdud@naver.com>
2023-06-25 23:55:26 +0900

    비즈니스 로직 엔티티 생성

---
+++ jpashopV2/src/main/java/jpabookv2/jpashop/domain/Member.java
@@ -0,0 +1,28 @@
+package jpabookv2.jpashop.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.*;
+
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany
+    private List<Order> orders = new ArrayList<>();
+
+}
