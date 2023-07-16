a432cf9855da8011fdf76c11ec7c3e3abb7f270e
JeongWonYoung <nicednjsdud@naver.com>
2023-07-16 23:26:54 +0900

    엔티티 전부 LAZY로 변경

--- jpashopV2/src/main/java/jpabookv2/jpashop/domain/Address.java
+++ jpashopV2/src/main/java/jpabookv2/jpashop/domain/Address.java
@@ -1,14 +1,22 @@
 package jpabookv2.jpashop.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@NoArgsConstructor
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
