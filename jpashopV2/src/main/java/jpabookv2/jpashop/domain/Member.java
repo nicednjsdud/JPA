987d88c3e7dbe79dd7b99acc921bbfda2d38ea5a
JeongWonYoung <nicednjsdud@naver.com>
2023-07-19 00:33:37 +0900

    엔티티 연관관계 메서드 설정

--- jpashopV2/src/main/java/jpabookv2/jpashop/domain/Member.java
+++ jpashopV2/src/main/java/jpabookv2/jpashop/domain/Member.java
@@ -25,4 +25,6 @@ public class Member {
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
+
+
 }
--- jpashopV2/src/main/java/jpabookv2/jpashop/domain/Order.java
+++ jpashopV2/src/main/java/jpabookv2/jpashop/domain/Order.java
@@ -22,14 +22,31 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
     private List<OrderItem> orderItemList = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    //==연관관계 메서드==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItemList.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
