acd7bfb5d475a1e3dc79195a1722db2fd9486196
JeongWonYoung <nicednjsdud@naver.com>
2023-08-03 00:44:37 +0900

    멤버 서비스 로직 구현

---
+++ jpashopV2/src/main/java/jpabookv2/jpashop/service/MemberService.java
@@ -0,0 +1,42 @@
+package jpabookv2.jpashop.service;
+
+import jpabookv2.jpashop.domain.Member;
+import jpabookv2.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional(readOnly = false)
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+
+}
