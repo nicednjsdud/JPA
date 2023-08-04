05d4d9489059321acad0ac530e6d77867561e77a
JeongWonYoung <nicednjsdud@naver.com>
2023-08-05 01:09:22 +0900

    주문 서비스 개발

--- jpashopV2/src/main/java/jpabookv2/jpashop/domain/OrderItem.java
+++ jpashopV2/src/main/java/jpabookv2/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabookv2.jpashop.domain;
 
 import jpabookv2.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -26,6 +29,8 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격
     private int count;      // 주문 수량
+
+
 
     // 생성 메서드
     public static OrderItem createOrderItem(Item item, int orderPrice, int count){
