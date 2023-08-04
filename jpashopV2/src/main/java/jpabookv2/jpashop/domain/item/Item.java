dc382e5b4cdbd2222d2b43d155311c798cf86f73
JeongWonYoung <nicednjsdud@naver.com>
2023-08-05 00:17:44 +0900

    재고 비즈니스 로직 구현

--- jpashopV2/src/main/java/jpabookv2/jpashop/domain/item/Item.java
+++ jpashopV2/src/main/java/jpabookv2/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabookv2.jpashop.domain.item;
 
 import jpabookv2.jpashop.domain.Category;
+import jpabookv2.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,18 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categoryList = new ArrayList<>();
+
+
+    // 비즈니스 로직
+    public void addStock(int stockQuantity){
+        this.stockQuantity += stockQuantity;
+    }
+
+    public void removeStock(int stockQuantity){
+        int restStock = this.stockQuantity - stockQuantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
