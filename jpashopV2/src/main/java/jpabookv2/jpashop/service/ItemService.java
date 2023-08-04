b4bd58b699d0706914906a49378dd32a636859c8
JeongWonYoung <nicednjsdud@naver.com>
2023-08-05 00:28:03 +0900

    아이템 서비스 생성 및 구현

---
+++ jpashopV2/src/main/java/jpabookv2/jpashop/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpabookv2.jpashop.service;
+
+import jpabookv2.jpashop.domain.item.Item;
+import jpabookv2.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.*;
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+    
+}
