24381c61771b41f9bbb7be077a8c1d02a4514cc8
JeongWonYoung <nicednjsdud@naver.com>
2023-08-10 00:48:03 +0900

    주문 동적 쿼리 생성

--- jpashopV2/src/main/java/jpabookv2/jpashop/repository/OrderRepository.java
+++ jpashopV2/src/main/java/jpabookv2/jpashop/repository/OrderRepository.java
@@ -1,11 +1,14 @@
 package jpabookv2.jpashop.repository;
 
 import java.util.*;
+
 import jpabookv2.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 @Repository
 @RequiredArgsConstructor
@@ -13,13 +16,52 @@ public class OrderRepository {
 
     private final EntityManager em;
 
-    public void save(Order order){
+    public void save(Order order) {
         em.persist(order);
     }
 
-    public Order findOne(Long id){
+    public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch)
+    public List<Order> findAll(OrderSearch orderSearch) {
+//        em.createQuery("select o from Order o join o.member m" +
+//                        " where o.status = : status" +
+//                        " and m.name like :name",Order.class)
+//                .setParameter("status",orderSearch.getOrderStatus())
+//                .setParameter("name",orderSearch.getMemberName())
+//                .setMaxResults(1000)
+//                .getResultList();
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+    }
 }
