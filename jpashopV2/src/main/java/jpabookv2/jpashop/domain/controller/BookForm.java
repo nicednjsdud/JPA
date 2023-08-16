ac9e43b0ef5f1ec26881e17dde569c34d9d70baa
JeongWonYoung <nicednjsdud@naver.com>
2023-08-16 17:15:49 +0900

    상품 등록 api 구현

---
+++ jpashopV2/src/main/java/jpabookv2/jpashop/domain/controller/BookForm.java
@@ -0,0 +1,18 @@
+package jpabookv2.jpashop.domain.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookForm {
+
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
+}
---
+++ jpashopV2/src/main/java/jpabookv2/jpashop/domain/controller/ItemController.java
@@ -0,0 +1,35 @@
+package jpabookv2.jpashop.domain.controller;
+
+import jpabookv2.jpashop.domain.item.Book;
+import jpabookv2.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form",new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form){
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
