fab224da04a8904a6c4771ed2fd9027e7fa8076c
JeongWonYoung <nicednjsdud@naver.com>
2023-08-16 17:10:50 +0900

    멤버 조회 api 생성

--- jpashopV2/src/main/java/jpabookv2/jpashop/domain/controller/MemberController.java
+++ jpashopV2/src/main/java/jpabookv2/jpashop/domain/controller/MemberController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import java.util.*;
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -37,5 +37,11 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        model.addAttribute("members", memberService.findMembers());
+        return "members/memberList";
     }
 }
